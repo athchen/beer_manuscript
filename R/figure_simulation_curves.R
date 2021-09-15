667d1afe73e64410a0d7924f55065ab99515c57e
Athena Chen <achen70@jhu.edu>
2021-09-14 21:21:23 -0400

    Added simulation output

--- R/figure_simulation_curves.R
+++ R/figure_simulation_curves.R
@@ -6,7 +6,6 @@
 #' 
 #' Code to generate figures:
 #' - simulation_roc_prc_interp.png
-#' - simulation_roc_prc_interp_all.png
 #' - simulation_roc_prc.png
 #' - simulation_roc_bybeads.png
 #' - simulation_prc_bybeads.png
@@ -112,69 +111,6 @@ roc_prc_interp <- ggarrange(roc_interp,
                             ncol = 1, nrow = 2)
 
 ggsave("figures/simulation_roc_prc_interp.png", roc_prc_interp, 
-       units = "in", height = 9, width = 7)
-
-# Figure: simulation_roc_prc_interp_all.png ------------
-roc_interp <- curves_summary %>%
-    ggplot(aes(x = x, y = mean_sens, group = method)) +
-    geom_line(aes(color = method), size = 0.5) +
-    facet_grid(num_beads ~ group_lab, 
-               labeller = labeller(group_lab = label_parsed, 
-                                   num_beads = c("2" = "2 beads",
-                                                 "4" = "4 beads", 
-                                                 "8" = "8 beads"))) +
-    coord_fixed(xlim = c(0, 1), ylim = c(0, 1)) +
-    labs(x = "1 - specificity", y = "sensitivity",
-         color = "method") +
-    scale_color_manual(breaks = c("BEER_truth", "BEER_mom", "BEER_mle", 
-                                  "BEER_edgeR", "edgeR"),
-                       labels = c("BEER, truth", "BEER, MOM", 
-                                  "BEER, MLE", "BEER, edgeR", 
-                                  "edgeR"), 
-                       values = c(brewer.pal(5, "Reds")[2:5],
-                                  brewer.pal(3, "Greys")[c(3)])) +
-    scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-    scale_x_continuous(breaks = seq(0, 1, 0.2)) +
-    theme_bw() +
-    theme(legend.title = element_text(size = 6), 
-          legend.text = element_text(size = 6), 
-          legend.key.size = unit(0.6, "lines"), 
-          legend.background = element_rect(color = "black", size = 0.3),  
-          legend.position = c(0.92, 0.81),
-          axis.title = element_text(size = 8), 
-          axis.text = element_text(size = 8), 
-          plot.margin = unit(c(0, 0, 0.25, 0), "cm"))
-
-prc_interp <- curves_summary %>%
-    ggplot(aes(x = x, y = mean_ppv, group = method)) +
-    geom_line(aes(color = method), size = 0.5) +
-    facet_grid(num_beads ~ group_lab, 
-               labeller = labeller(group_lab = label_parsed, 
-                                   num_beads = c("2" = "2 beads",
-                                                 "4" = "4 beads", 
-                                                 "8" = "8 beads"))) +
-    coord_fixed(xlim = c(0, 1), ylim = c(0, 1)) +
-    labs(x = "sensitivity", y = "positive predictive value (PPV)",
-         color = "method") +
-    scale_color_manual(breaks = c("BEER_truth", "BEER_mom", "BEER_mle", 
-                                  "BEER_edgeR", "edgeR"),
-                       labels = c("BEER, truth", "BEER, MOM", 
-                                  "BEER, MLE", "BEER, edgeR", 
-                                  "edgeR"), 
-                       values = c(brewer.pal(5, "Reds")[2:5],
-                                  brewer.pal(3, "Greys")[c(3)])) +
-    scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-    scale_x_continuous(breaks = seq(0, 1, 0.2)) +
-    theme_bw() +
-    theme(axis.text = element_text(size = 8), 
-          axis.title = element_text(size = 8), 
-          plot.margin = unit(c(0, 0, 0.25, 0), "cm"))
-
-roc_prc_interp <- ggarrange(roc_interp, 
-                            prc_interp + theme(legend.position = "none"), 
-                            ncol = 1, nrow = 2)
-
-ggsave("figures/simulation_roc_prc_interp_all.png", roc_prc_interp, 
        units = "in", height = 9, width = 7)
 
 # Figure: simulation_roc_prc.png ------------
